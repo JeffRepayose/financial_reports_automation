c8bb034a41d58152c5cd7fd7deb2fb758e8ecbf1
Jennifer <jennifersequina@gmail.com>
2021-08-29 17:12:18 +0400

    updated totals

--- pre_processing.py
+++ pre_processing.py
@@ -4,6 +4,7 @@ import numpy as np
 
 gl_codes = pd.read_csv('data/gl_codes.csv')
 
+
 def pre_process_data(data):
     concat_data = data.merge(gl_codes, on='Account', how='left')
     if is_string_dtype(concat_data['Amount in local currency']):
@@ -11,3 +12,10 @@ def pre_process_data(data):
         concat_data['Amount in local currency'] = (concat_data['Amount in local currency'].astype(np.float64).round(2))
     processed_data = concat_data
     return processed_data
+
+
+# def as_currency(amount):
+#     if amount >= 0:
+#         return '${:,.2f}'.format(amount)
+#     else:
+#         return '-${:,.2f}'.format(-amount)
--- reports.py
+++ reports.py
@@ -12,6 +12,8 @@ def get_balance_sheet(data):
                            margins_name='YTD Total')
     bs = pivot.loc[['ASSET', 'LIABILITIES', 'YTD Total']].replace(np.NaN, 0)
     balance_sheet = bs.rename(index={'YTD Total': 'EQUITIES'})
+    bs_total = np.round(balance_sheet.sum(axis=0), 2).to_list()
+    balance_sheet.loc['EQUITIES'] = bs_total
     return balance_sheet
 
 
@@ -25,6 +27,8 @@ def get_gross_margin(data):
                            margins_name='YTD Total')
     gm = pivot.loc[['Sales', 'Cost of Sales', 'YTD Total']].replace(np.NaN, 0)
     gross_margin = gm.rename(index={'YTD Total': 'Gross Margin'})
+    gm_total = np.round(gross_margin.sum(axis=0), 2).to_list()
+    gross_margin.loc['Gross Margin'] = gm_total
     return gross_margin
 
 
@@ -38,4 +42,6 @@ def get_profit_loss(data):
                             margins_name='YTD Total')
     pnl = pivot.loc[['Gross Margin', 'Operating Expenses', 'YTD Total']].replace(np.NaN, 0)
     profit_loss = pnl.rename(index={'YTD Total': 'Net Profit'})
+    pnl_total = np.round(profit_loss.sum(axis=0), 2).to_list()
+    profit_loss.loc['Net Profit/Loss'] = pnl_total
     return profit_loss
