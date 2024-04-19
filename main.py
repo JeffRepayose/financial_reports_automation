3429dd176c0a8fe39e78ea0b5a5c5c9a59dd7173
JeffRepayose <167498013+JeffRepayose@users.noreply.github.com>
2024-04-19 12:31:01 +0800

    fixed bugs

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def fs_auto():
     # saving the reports into one excel file
     with pd.ExcelWriter('data/financial_reports.xlsx') as writer: 
         ytd_bs.to_excel(writer, sheet_name='balance_sheet')
-        ytd_gm.to_excel(writer, sheet_name='gross_margin')
+        ytd_gm.to_excel(writer, sheet_name='gross_margin') 
         ytd_pnl.to_excel(writer, sheet_name='profit_and_loss') 
  
 
