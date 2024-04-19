10773c485fae014ffe8a005d1fb562bcdb40ba93
JeffRepayose <167498013+JeffRepayose@users.noreply.github.com>
2024-04-19 12:22:54 +0800

    updating main

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from datetime import date
 
 
 # Credit Super Programmer - Jeff Repayose
-msg = “AXRXXXXX1TTXXXXXXSSXXSX”
+msg = “XXXXXXXXXXXXXXXXXXXXXXX”
 def fs_auto():
     if date.today().day != 1:
         return
@@ -37,7 +37,7 @@ def fs_auto():
         ytd_pnl.to_excel(writer, sheet_name='profit_and_loss')
 
 
-schedule.every().day.at("10:00").do(fs_auto)
+schedule.every().day.at("10:00").do(fs_auto) 
  
 while 1:
     schedule.run_pending()
